98d215495768619e816844e9e3ec6c49d90f9062
Chinmay Kulkarni <30969403+Grandolf49@users.noreply.github.com>
2020-05-03 11:45:25 +0530

    updated cpp template

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+
+#define INF (int)1e9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+
 typedef long long ll;
 typedef unsigned long long ull;
 
