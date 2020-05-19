1a05a94b700126f678be18d5485b60d2a49b8361
Chinmay Kulkarni <30969403+Grandolf49@users.noreply.github.com>
2020-05-19 22:52:31 +0530

    added fast input to java template

--- Main.java
+++ Main.java
@@ -1,34 +1,121 @@
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
-import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Main {
 
     static final File ip = new File("input.txt");
     static final File op = new File("output.txt");
-    static Scanner scan;
-    static PrintStream print;
+    static InputReader in;
 
-    /**
-     * All IO operations are perfored from ip and op files. Use scan.hasNext()
-     * function before accepting input from Scanner object.
+    static {
+        try {
+            System.setOut(new PrintStream(op));
+            System.setIn(new FileInputStream(ip));
+        } catch (Exception e) {
+        }
+
+        in = new InputReader(System.in);
+    }
+
+    /***
+     * Write your code in this function! Input: Use InputReader "in" for reading
+     * data. Output: Use System.out.println() to write output to file
      */
     private static void solve(int t1) {
-        System.out.println(t1);
+        int n = in.readInt();
+        System.out.println(n);
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        scan = new Scanner(ip);
-        print = new PrintStream(op);
-        System.setOut(print);
 
-        int t = scan.nextInt(), t1 = 1;
-        // scan.nextLine();
+        int t = in.readInt();
+        int t1 = 1;
         while (t1 <= t) {
             solve(t1);
             t1++;
         }
+    }
 
+    /**
+     * A class for Fast Input
+     */
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int readInt() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String readString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next() {
+            return readString();
+        }
+
+    }
+
+    public interface SpaceCharFilter {
+        public boolean isSpaceChar(int ch);
     }
 }
